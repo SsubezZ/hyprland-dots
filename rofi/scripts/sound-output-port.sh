3bd8c01e64bb2fb59d4d3af5afceba6f3d81bb22
SsubezZ <subez.poudel1@gmail.com>
2024-02-14 16:40:11 +0545

    Audio Port selection revamp

--- rofi/scripts/sound-output-port.sh
+++ rofi/scripts/sound-output-port.sh
@@ -1,15 +1,13 @@
 !/usr/bin/env bash
 IFS=$'\n'
 
-if [ "$@" ]
-then
-    desc="$*"
-    device=$(pactl list sinks | grep "$desc" | grep "output" | grep "type" | awk '{print $1}' | cut -d ":" -f1 | xargs)
-    pactl set-sink-port 0 "$device"
+if [ "$@" ]; then
+	desc="$*"
+	device=$(pactl list sinks | grep "$desc" | grep "output" | grep "type" | awk '{print $1}' | cut -d ":" -f1 | xargs)
+	pactl set-sink-port 0 "$device"
 else
-    echo -en "\x00prompt\x1fAudio Output\n"
-    for x in $(pactl list sinks | grep "output" | grep "type" | awk '{print $2}')
-    do
-        echo "$x"|xargs
-    done
+	echo -en "\x00prompt\x1fOutput Port\n"
+	for x in $(pactl list sinks | grep "output" | grep "type" | awk '{print $2}'); do
+		echo "$x" | xargs
+	done
 fi
