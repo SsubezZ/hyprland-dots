5e283c93a048366df486cf680333e87f28b00aa3
SsubezZ <subez.poudel1@gmail.com>
2024-04-02 18:18:19 +0545

    full portal location added

--- hypr/scripts/screen-share.sh
+++ hypr/scripts/screen-share.sh
@@ -2,5 +2,5 @@
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-xdg-desktop-portal-hyprland &
-xdg-desktop-portal &
+/usr/lib/xdg-desktop-portal-hyprland &
+/usr/lib/xdg-desktop-portal-hyprland &
