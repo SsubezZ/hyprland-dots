d155109f30e051410c4df3b8c1848537f89fff37
SsubezZ <subez.poudel1@gmail.com>
2024-07-06 21:12:51 +0545

    rofi: massive revamp
    implemented dynamic height(not fixed anymore)
    reworked the power-menu
    used #! insted of ! for shebang
    font fixes
    cleanups and other fixes

--- rofi/scripts/sound-input-port.sh
+++ rofi/scripts/sound-input-port.sh
@@ -1,13 +1,13 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 IFS=$'\n'
 
 if [ "$@" ]; then
-	desc="$*"
-	device=$(pactl list sources | grep "$desc" | grep "output" | grep "type" | awk '{print $1}' | cut -d ":" -f1 | xargs)
-	pactl set-source-port 0 "$device"
+  desc="$*"
+  device=$(pactl list sources | grep "$desc" | grep "output" | grep "type" | awk '{print $1}' | cut -d ":" -f1 | xargs)
+  pactl set-source-port 0 "$device"
 else
-	echo -en "\x00prompt\x1fInput Port\n"
-	for x in $(pactl list sources | grep 'input' | grep "type" | awk '{print $2}'); do
-		echo "$x" | xargs
-	done
+  echo -en "\x00prompt\x1fInput Port\n"
+  for x in $(pactl list sources | grep 'input' | grep "type" | awk '{print $2}'); do
+    echo "$x" | xargs
+  done
 fi
