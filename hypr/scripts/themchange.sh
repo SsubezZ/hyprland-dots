7d3620d7148c434885dc3da5177a1477d4f836ea
SsubezZ <subez.poudel1@gmail.com>
2024-08-08 22:23:34 +0545

    themechanger: added rofi & added variables for eoa

--- hypr/scripts/themchange.sh
+++ hypr/scripts/themchange.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
 
 CURRENT=$(gsettings get org.gnome.desktop.interface color-scheme | cut -d "'" -f2 | xargs)
-LIGHT="Colloid-Light"
-DARK="Colloid-Dark"
+GTK_LIGHT="Colloid-Light"
+GTK_DARK="Colloid-Dark"
+
+pkill rofi
+
 if [ "$CURRENT" = "prefer-dark" ]; then
   gsettings set org.gnome.desktop.interface color-scheme prefer-light
-  gsettings set org.gnome.desktop.interface gtk-theme $LIGHT
+  gsettings set org.gnome.desktop.interface gtk-theme $GTK_LIGHT
   gsettings set org.gnome.desktop.interface icon-theme Papirus-Light
-  # rm $HOME/.config/gtk-4.0
-  # ln -s /home/subez/.themes/Graphite/gtk-4.0 /home/subez/.config/gtk-4.0
   sed -i "s/dark/light/" ~/.config/alacritty/alacritty.toml
-  sed -i "s/$DARK/$LIGHT/" ~/.config/xsettingsd/xsettingsd.conf
+  sed -i "s/$GTK_DARK/$GTK_LIGHT/" ~/.config/xsettingsd/xsettingsd.conf
+  sed -i "s/dark/light/" ~/.config/rofi/theme.rasi ~/.config/rofi/scripts/rofi-polkit-agent.rasi
   hyprctl --batch "\
     keyword windowrulev2 opacity 1 override 1 override 1 override class:(.*);\
     keyword general:col.active_border rgb(0F0F0F);\
@@ -18,12 +20,11 @@ if [ "$CURRENT" = "prefer-dark" ]; then
   notify-send --app-name "Theme" "Theme Changed" "Switched to Light" -e -h string:x-canonical-private-synchronous:themechange
 else
   gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-  gsettings set org.gnome.desktop.interface gtk-theme $DARK
+  gsettings set org.gnome.desktop.interface gtk-theme $GTK_DARK
   gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
-  # rm $HOME/.config/gtk-4.0
-  # ln -s /home/subez/.themes/Graphite-dark/gtk-4.0 /home/subez/.config/gtk-4.0
   sed -i "s/light/dark/" ~/.config/alacritty/alacritty.toml
-  sed -i "s/$LIGHT/$DARK/" ~/.config/xsettingsd/xsettingsd.conf
+  sed -i "s/$GTK_LIGHT/$GTK_DARK/" ~/.config/xsettingsd/xsettingsd.conf
+  sed -i "s/light/dark/" ~/.config/rofi/theme.rasi ~/.config/rofi/scripts/rofi-polkit-agent.rasi
   hyprctl reload
   notify-send --app-name "Theme" "Theme Changed" "Switched to Dark" -e -h string:x-canonical-private-synchronous:themechange
 fi
