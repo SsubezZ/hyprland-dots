a4b6486186fe52e8ebd48584f7c5d361270b89e7
SsubezZ <subez.poudel1@gmail.com>
2024-08-09 22:10:14 +0545

    themechanger: improved name replacement
    replace the second word despite whatever it may be

--- hypr/scripts/themchange.sh
+++ hypr/scripts/themchange.sh
@@ -3,6 +3,8 @@
 CURRENT=$(gsettings get org.gnome.desktop.interface color-scheme | cut -d "'" -f2 | xargs)
 GTK_LIGHT="Colloid-Light"
 GTK_DARK="Colloid-Dark"
+# GTK_LIGHT="Graphite-Light"
+# GTK_DARK="Graphite-Dark"
 
 pkill rofi
 
@@ -11,7 +13,7 @@ if [ "$CURRENT" = "prefer-dark" ]; then
   gsettings set org.gnome.desktop.interface gtk-theme $GTK_LIGHT
   gsettings set org.gnome.desktop.interface icon-theme Papirus-Light
   sed -i "s/dark/light/" ~/.config/alacritty/alacritty.toml
-  sed -i "s/$GTK_DARK/$GTK_LIGHT/" ~/.config/xsettingsd/xsettingsd.conf
+  sed -i "1s/[^ ]*[^ ]/\"$GTK_LIGHT\"/2" ~/.config/xsettingsd/xsettingsd.conf
   sed -i "s/dark/light/" ~/.config/rofi/theme.rasi ~/.config/rofi/scripts/rofi-polkit-agent.rasi
   hyprctl --batch "\
     keyword windowrulev2 opacity 1 override 1 override 1 override class:(.*);\
@@ -23,7 +25,7 @@ else
   gsettings set org.gnome.desktop.interface gtk-theme $GTK_DARK
   gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
   sed -i "s/light/dark/" ~/.config/alacritty/alacritty.toml
-  sed -i "s/$GTK_LIGHT/$GTK_DARK/" ~/.config/xsettingsd/xsettingsd.conf
+  sed -i "1s/[^ ]*[^ ]/\"$GTK_DARK\"/2" ~/.config/xsettingsd/xsettingsd.conf
   sed -i "s/light/dark/" ~/.config/rofi/theme.rasi ~/.config/rofi/scripts/rofi-polkit-agent.rasi
   hyprctl reload
   notify-send --app-name "Theme" "Theme Changed" "Switched to Dark" -e -h string:x-canonical-private-synchronous:themechange
