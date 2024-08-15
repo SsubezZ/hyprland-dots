f0849bca3b20aad2d09121ccebdfa81cc78a4d01
SsubezZ <subez.poudel1@gmail.com>
2024-08-15 14:25:40 +0545

    nvim: revamped dashboard with filled icons, new logo etc

--- nvim/plugins/dashboard-nvim.lua
+++ nvim/plugins/dashboard-nvim.lua
@@ -2,15 +2,14 @@ return {
   "nvimdev/dashboard-nvim",
   opts = function()
     local logo = [[
-      ┏━┓ ┏┓┏━━━┓┏━━━┓┏┓  ┏┓┏━━┓┏━┓┏━┓      
-      ┃ ┗┓┃┃┃┏━━┛┃┏━┓┃┃┗┓┏┛┃┗┫┣┛┃ ┗┛ ┃      
-      ┃┏┓┗┛┃┃┗━━┓┃┃ ┃┃┗┓┃┃┏┛ ┃┃ ┃┏┓┏┓┃      
-      ┃┃┗┓ ┃┃┏━━┛┃┃ ┃┃ ┃┗┛┃  ┃┃ ┃┃┃┃┃┃      
-      ┃┃ ┃ ┃┃┗━━┓┃┗━┛┃ ┗┓┏┛ ┏┫┣┓┃┃┃┃┃┃      
-      ┗┛ ┗━┛┗━━━┛┗━━━┛  ┗┛  ┗━━┛┗┛┗┛┗┛      
+███    ██ ███████  ██████  ██    ██ ██ ███    ███
+████   ██ ██      ██    ██ ██    ██ ██ ████  ████
+██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██
+██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██
+██   ████ ███████  ██████    ████   ██ ██      ██
     ]]
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
+    logo = string.rep("\n", 10) .. logo .. "\n"
 
     local opts = {
       theme = "doom",
@@ -24,27 +23,27 @@ return {
         -- stylua: ignore
         center = {
           { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-          { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-          { action = "cd ~/Gits | Neotree",                            desc = " Git folder",      icon = "󰊢 ", key = "g" },
-          { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "i" },
-          { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
+          { action = "ene | startinsert",                              desc = " New File",        icon = "󰈔 ", key = "n" },
+          { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = "󰈢 ", key = "r" },
+          { action = "cd ~/Gits | Neotree",                            desc = " Git Folder",      icon = "󰊢 ", key = "g" },
+          { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "i" },
+          { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
           { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = "cd ~/Gits/hyprland-dots | Neotree",              desc = " hyprland-dots",   icon = " ", key = "h" },
-          { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+          { action = "cd ~/Gits/hyprland-dots | Neotree",              desc = " hyprland-dots",   icon = " ", key = "h" },
+          { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
       },
         footer = function()
           local stats = require("lazy").stats()
           local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          return { "󱐋 Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
         end,
       },
     }
 
     for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+      button.desc = button.desc .. string.rep(" ", 40 - #button.desc)
       button.key_format = "  %s"
     end
 
