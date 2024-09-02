30b3c2d03868a81944f48a41bda9056fcf1101ad
SsubezZ <subez.poudel1@gmail.com>
2024-09-02 16:37:23 +0545

    gamemode.sh: implemented hyprpaper toggle

--- hypr/scripts/gamemode.sh
+++ hypr/scripts/gamemode.sh
@@ -3,6 +3,7 @@
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 
 if [ "$HYPRGAMEMODE" = 1 ]; then
+  pkill hyprpaper
   hyprctl --batch "\
     keyword animations:enabled 0;\
     keyword decoration:drop_shadow 0;\
@@ -13,6 +14,7 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
     # keyword decoration:rounding 0"
   notify-send --app-name "Gamemode" "Gamemode Enabled" "Decorations Diasabled" -e -h string:x-canonical-private-synchronous:gamemode
 else
+  hyprpaper &
   hyprctl reload
   notify-send --app-name "Gamemode" "Gamemode Disabled" "Decorations Enabled" -e -h string:x-canonical-private-synchronous:gamemode
 fi
