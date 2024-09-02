1fb9e9a2da9ed2ab707cd3afc92aaf9cb3eba01a
SsubezZ <subez.poudel1@gmail.com>
2024-09-02 17:22:02 +0545

    theming: massive revamp & overhaul
    revamped themechange.sh
    added themeupdate.sh [gtk theme installer]
    implemented and/or fixed alacritty, hyprland, rofi(also jerry),
    yazi(somewhat)

---
+++ hypr/scripts/themechange.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+declare -A GTK_THEMES=(["light"]="Colloid-Light" ["dark"]="Colloid-Dark")
+declare -A ICN_THEMES=(["light"]="Papirus-Light" ["dark"]="Papirus-Dark")
+declare -A CUR_THEMES=(["light"]="Graphite-dark-cursors" ["dark"]="Graphite-light-cursors")
+
+declare -A ALACRITTY_THEMES=(["light"]="theme-light.toml" ["dark"]="theme-dark.toml")
+declare -A ROFI_THEMES=(["light"]="theme-light.rasi" ["dark"]="theme-dark.rasi")
+declare -A HYPR_THEMES=(["light"]="theme-light.conf" ["dark"]="theme-dark.conf")
+# declare -A YAZI_THEMES=(["light"]="theme-light.toml" ["dark"]="theme-dark.toml")
+declare -A SPICETIFY_THEMES=(["light"]="light" ["dark"]="dark")
+
+pre() {
+  echo "Cleaning up previous GTK 4.0 configuration..."
+  rm -rf ~/.config/gtk-4.0 &>/dev/null
+  echo "Killing any running Rofi instances..."
+  pkill -x rofi &>/dev/null
+  # if pgrep -x spotify >/dev/null; then
+  #   echo "Starting Spicetify watch..."
+  #   spicetify watch -s &>/dev/null &
+  #   sleep 1 && pkill -x spicetify &>/dev/null
+  # fi
+}
+
+set_theme() {
+  local mode=$1
+
+  echo "Setting theme to $mode mode..."
+  gsettings set org.gnome.desktop.interface color-scheme "prefer-$mode" &>/dev/null
+  gsettings set org.gnome.desktop.interface gtk-theme "${GTK_THEMES[$mode]}" &>/dev/null
+  gsettings set org.gnome.desktop.interface icon-theme "${ICN_THEMES[$mode]}" &>/dev/null
+  gsettings set org.gnome.desktop.interface cursor-theme "${CUR_THEMES[$mode]}" &>/dev/null
+  hyprctl setcursor "${CUR_THEMES[$mode]}" 24 &>/dev/null
+
+  echo "Updating xsettingsd configuration..."
+  sed -i -e "/^Net\/ThemeName/s/\"[^\"]*\"/\"${GTK_THEMES[$mode]}\"/" \
+    -e "/^Net\/IconThemeName/s/\"[^\"]*\"/\"${ICN_THEMES[$mode]}\"/" \
+    -e "/^Gtk\/CursorThemeName/s/\"[^\"]*\"/\"${CUR_THEMES[$mode]}\"/" \
+    ~/.config/xsettingsd/xsettingsd.conf &>/dev/null
+
+  echo "Applying Alacritty theme..."
+  sed -i "/^import = \[/s|\(\"[^\"]*theme-[^\"]*\"\)|\"~/.config/alacritty/${ALACRITTY_THEMES[$mode]}\"|" ~/.config/alacritty/alacritty.toml &>/dev/null
+
+  echo "Applying Rofi theme..."
+  sed -i "/^@theme .*theme-.*\.rasi/s|@theme \".*\"|@theme \"${ROFI_THEMES[$mode]}\"|" ~/.config/rofi/colorscheme.rasi &>/dev/null
+
+  echo "Applying Hyprland theme..."
+  sed -i "/^source = .*theme-.*\.conf/s|source = .*|source = ./themes/${HYPR_THEMES[$mode]}|" ~/.config/hypr/hyprland.conf &>/dev/null
+
+  # echo "Applying Yazi theme..."
+  # cp "$HOME/.config/yazi/${YAZI_THEMES[$mode]}" ~/.config/yazi/theme.toml &>/dev/null
+
+  echo "Applying Spicetify theme..."
+  spicetify config color_scheme "${SPICETIFY_THEMES[$mode]}" &>/dev/null
+  spicetify apply -n &>/dev/null
+}
+
+post() {
+  echo "Reloading xsettingsd..."
+  killall -HUP xsettingsd &>/dev/null
+  echo "Reloading Hyprland..."
+  hyprctl reload &>/dev/null
+  echo "Reloading Yazi..."
+  # yazi --reload &>/dev/null
+  if pgrep -x spotify >/dev/null; then
+    echo "Restarting Spicetify watch..."
+    spicetify watch -s &>/dev/null &
+    sleep 1 && pkill -x spicetify &>/dev/null
+  fi
+}
+
+pre
+CURRENT=$(gsettings get org.gnome.desktop.interface color-scheme | tr -d "'")
+if [[ "$CURRENT" == "prefer-dark" ]]; then
+  set_theme "light"
+else
+  set_theme "dark"
+fi
+post
+
+exit
---
+++ hypr/scripts/themeupdate.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+suppress_output() {
+  "$@" >/dev/null 2>&1
+}
+
+echo
+echo "Choose a GTK theme:"
+echo "1) Colloid"
+echo "2) Graphite"
+read -r -p "Enter your choice (1 or 2, Default: 1): " THEME_CHOICE
+[ -z "$THEME_CHOICE" ] && THEME_CHOICE=1
+
+if [ "$THEME_CHOICE" -eq 2 ]; then
+  THEME_NAME="Graphite"
+  GIT_URL="https://github.com/vinceliuice/Graphite-gtk-theme.git"
+
+  echo
+  read -r -p "Specify accent variant [blue|purple|pink|red|orange|yellow|green|teal|grey] (Default: grey): " ACCENT
+  [ -z "$ACCENT" ] && ACCENT="default"
+
+  echo
+  read -r -p "Specify size variant [standard|compact] (Default: compact): " SIZE
+  [ -z "$SIZE" ] && SIZE="compact"
+
+  echo
+  read -r -p "Enter colorscheme [black|nord] (Default: black): " COLORSCHEME
+  [ -z "$COLORSCHEME" ] && COLORSCHEME="black"
+
+  echo
+  read -r -p "Enter tweak(s) for $THEME_NAME [darker|rimless|normal|float|colorful] (Default: rimless normal): " TWEAKS
+  [ -z "$TWEAKS" ] && TWEAKS="rimless normal"
+else
+  THEME_NAME="Colloid"
+  GIT_URL="https://github.com/vinceliuice/Colloid-gtk-theme.git"
+
+  echo
+  read -r -p "Specify accent variant [blue|purple|pink|red|orange|yellow|green|teal|grey] (Default: grey): " ACCENT
+  [ -z "$ACCENT" ] && ACCENT="grey"
+
+  echo
+  read -r -p "Specify size variant [standard|compact] (Default: compact): " SIZE
+  [ -z "$SIZE" ] && SIZE="compact"
+
+  echo
+  read -r -p "Enter colorscheme [black|nord|dracula|gruvbox|everforest|catppuccin] (Default: black): " COLORSCHEME
+  [ -z "$COLORSCHEME" ] && COLORSCHEME="black"
+
+  echo
+  read -r -p "Enter tweak(s) for $THEME_NAME [rimless|normal|float] (Default: rimless normal): " TWEAKS
+  [ -z "$TWEAKS" ] && TWEAKS="rimless normal"
+fi
+
+BASE_DIR="$HOME/Programs/Themes/$THEME_NAME"
+THEME_DIR="$HOME/.themes/"
+
+echo
+echo "Theme: $THEME_NAME"
+echo "Accent: $ACCENT"
+echo "Size: $SIZE"
+echo "Tweaks: $TWEAKS"
+echo "Colorscheme: $COLORSCHEME"
+
+echo
+echo "Cleaning up old theme directories..."
+suppress_output rm -rf "$BASE_DIR"
+suppress_output rm -rf "${THEME_DIR:?}/${THEME_NAME:?}"*
+
+echo
+echo "Cloning the theme repository..."
+if suppress_output git clone "$GIT_URL" "$BASE_DIR"; then
+  echo "Successfully cloned $GIT_URL"
+else
+  echo "Failed to clone repository. Exiting."
+  exit 1
+fi
+
+echo
+echo "Creating build directory..."
+suppress_output mkdir -p "$BASE_DIR/build"
+
+IFS=' ' read -r -a TWEAKS_ARRAY <<<"$TWEAKS"
+echo
+echo "Installing theme with selected options..."
+if suppress_output "$BASE_DIR/install.sh" -d "$BASE_DIR/build" -t "$ACCENT" -s "$SIZE" --tweaks "$COLORSCHEME" --tweaks "${TWEAKS_ARRAY[@]}"; then
+  echo "Installation successful."
+else
+  echo "Installation failed. Exiting."
+  exit 1
+fi
+
+echo
+echo "Renaming theme files..."
+cd "$BASE_DIR/build" || exit 1
+find . -depth -name "$THEME_NAME*" | while read -r path; do
+  new_path=$(dirname "$path")/$(
+    basename "$path" | sed -e 's/\(Blue\|Purple\|Pink\|Red\|Orange\|Yellow\|Green\|Teal\|Grey\|Compact\|compact\)-//gI' \
+      -e 's/-\(Nord\|Dracula\|Gruvbox\|Everforest\|Catppuccin\|Compact\|compact\)//gI'
+  )
+
+  if [ "$path" != "$new_path" ]; then
+    mkdir -p "$(dirname "$new_path")"
+    suppress_output mv "$path" "$new_path"
+    echo "Renamed $path to $new_path"
+  fi
+done
+
+echo
+echo "Copying themes to the themes directory..."
+suppress_output cp -r $THEME_NAME* "$THEME_DIR"
+
+if [[ "$(suppress_output gsettings get org.gnome.desktop.interface color-scheme | cut -d "'" -f2 | xargs)" == "prefer-light" ]]; then
+  THEME_NAME="$THEME_NAME"-Light
+  TEMP_THEME="Adwaita"
+  THEME_COLOR="prefer-light"
+
+else
+  THEME_NAME="$THEME_NAME"-Dark
+  TEMP_THEME="Adwaita-dark"
+  THEME_COLOR="prefer-dark"
+fi
+
+echo
+echo "Temporarily switching to Adwaita theme to force theme reapplication..."
+suppress_output gsettings set org.gnome.desktop.interface gtk-theme "$TEMP_THEME"
+suppress_output gsettings set org.gnome.desktop.interface color-scheme "$THEME_COLOR"
+sleep 1
+
+if [ -d "$THEME_DIR/$THEME_NAME" ]; then
+  echo "Theme directory exists. Proceeding to apply theme."
+else
+  echo "Theme directory not found. Exiting."
+  exit 1
+fi
+
+echo
+echo "Applying GTK theme..."
+suppress_output gsettings set org.gnome.desktop.interface gtk-theme "$THEME_NAME"
+
+echo "Restarting xsettingsd to apply the theme..."
+suppress_output killall -q xsettingsd
+suppress_output xsettingsd &
+disown
+
+echo "Theme installation complete. Exiting."
+exit
