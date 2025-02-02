c0dc5328e9ab1554ef2eb37495e3956d201be12f
SsubezZ <subez.poudel1@gmail.com>
2025-02-02 22:48:38 +0545

    scripts/screen-share: added & opperands, fixed screen-share(mostly)
    defaulted to mute source on launch

--- hypr/scripts/screen-share.sh
+++ hypr/scripts/screen-share.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
+
+killall -e xdg-desktop-portal-hyprland &
+killall -e xdg-desktop-portal-gtk &
+killall xdg-desktop-portal &
+
+sleep 1
+
 /usr/lib/xdg-desktop-portal-hyprland &
 systemctl restart --user pipewire.service &
+systemctl restart --user xdg-desktop-portal-hyprland &
+
+sleep 1
+
+wpctl set-volume @DEFAULT_SOURCE@ 25%
+wpctl set-mute @DEFAULT_SOURCE@ 1
