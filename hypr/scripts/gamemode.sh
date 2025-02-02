3bba71c2c096afedbab450c029028902ac5916b2
SsubezZ <subez.poudel1@gmail.com>
2025-02-02 22:38:31 +0545

    scripts/gamemode: ditched tmp file approach for blur checking

--- hypr/scripts/gamemode.sh
+++ hypr/scripts/gamemode.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env sh
 
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-
+HYPRGAMEMODE=$(hyprctl getoption decoration:blur:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ]; then
-  pkill hyprpaper
-  hyprctl --batch "\
-    keyword animations:enabled 0;\
-    keyword decoration:drop_shadow 0;\
-    keyword decoration:blur:enabled 0;\
-    # keyword general:gaps_in 0;\
-    # keyword general:gaps_out 0;\
-    # keyword general:border_size 1;\
-    # keyword decoration:rounding 0"
-  notify-send --app-name "Gamemode" "Gamemode Enabled" "Decorations Diasabled" -e -h string:x-canonical-private-synchronous:gamemode
-  hyprpm enable csgo-vulkan-fix
-else
-  hyprpaper &
-  hyprctl reload
-  notify-send --app-name "Gamemode" "Gamemode Disabled" "Decorations Enabled" -e -h string:x-canonical-private-synchronous:gamemode
-  hyprpm disable csgo-vulkan-fix
+	hyprctl keyword misc:disable_autoreload 1
+	pkill hyprpaper
+	hyprctl --batch "\
+    # keyword animations:enabled 0;\
+    keyword windowrulev2 forcergbx, class:.*;\
+    keyword windowrulev2 opaque, class:.*;\
+    keyword decoration:shadow:enabled 0;\
+    # keyword decoration:rounding 0;\
+    keyword decoration:blur:enabled 0"
+	swayosd-client --custom-message " Gamemode Enabled " || notify-send --app-name "Gamemode" "Gamemode Enabled" "Decorations Disabled" -e -h string:x-canonical-private-synchronous:gamemode
+	# hyprpm enable csgo-vulkan-fix
+	exit
 fi
 
+hyprpaper &
+hyprctl reload
+swayosd-client --custom-message " Gamemode Disabled " || notify-send --app-name "Gamemode" "Gamemode Disabled" "Decorations Enabled" -e -h string:x-canonical-private-synchronous:gamemode
+# hyprpm disable csgo-vulkan-fix
+hyprctl dismissnotify
+sleep 0.5
 exit
