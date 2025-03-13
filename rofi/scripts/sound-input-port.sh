6dc8960c72b1d0425e441aab82c6abe0543d6bf4
SsubezZ <subez.poudel1@gmail.com>
2025-03-13 22:40:05 +0545

    rofi/sound*: inactive port on topmost
    notification changes for non port
    mode-switcher changes

--- rofi/scripts/sound-input-port.sh
+++ rofi/scripts/sound-input-port.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 IFS=$'\n'
 
-if [ "$@" ]; then
-  desc="$*"
-  device=$(pactl list sources | grep "$desc" | grep "input" | grep "type" | awk '{print $1}' | cut -d ":" -f1 | xargs)
-  pactl set-source-port 0 "$device"
+source_list=$(pactl list sources)
+
+ports_info=$(echo "$source_list" | grep "input" | grep "type" | awk '{id=$1; sub(":", "", id); print id, $2}')
+
+active=$(echo "$source_list" | grep "Active Port:" | awk '{print $3}' | head -n 1)
+
+active_desc=$(echo "$ports_info" | awk -v id="$active" '$1 == id {print $2}')
+
+inactive=$(echo "$ports_info" | awk -v active="$active_desc" '$2 != active {print $2}')
+
+if [ "$#" -gt 0 ]; then
+	desc="$*"
+	device=$(echo "$ports_info" | grep "$desc" | awk '{print $1}' | head -n 1 | xargs)
+	pactl set-source-port 0 "$device"
 else
-  echo -en "\x00prompt\x1fInput Port\n"
-  for x in $(pactl list sources | grep 'input' | grep "type" | awk '{print $2}'); do
-    echo "$x" | xargs
-  done
+	echo -en "\x00prompt\x1fInput Port\n"
+	for port in $inactive; do
+		echo "$port"
+	done
+	echo "$active_desc"
 fi
--- rofi/scripts/sound-output-port.sh
+++ rofi/scripts/sound-output-port.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 IFS=$'\n'
 
-if [ "$@" ]; then
+sink_list=$(pactl list sinks)
+
+ports_info=$(echo "$sink_list" | grep "output" | grep "type" | awk '{id=$1; sub(":", "", id); print id, $2}')
+
+active=$(echo "$sink_list" | grep "Active Port:" | awk '{print $3}' | head -n 1)
+
+active_desc=$(echo "$ports_info" | awk -v id="$active" '$1 == id {print $2}')
+
+inactive=$(echo "$ports_info" | awk -v active="$active_desc" '$2 != active {print $2}')
+
+if [ "$#" -gt 0 ]; then
 	desc="$*"
-	device=$(pactl list sinks | grep "$desc" | grep "output" | grep "type" | awk '{print $1}' | cut -d ":" -f1 | xargs)
+	device=$(echo "$ports_info" | grep "$desc" | awk '{print $1}' | head -n 1 | xargs)
 	pactl set-sink-port 0 "$device"
 else
 	echo -en "\x00prompt\x1fOutput Port\n"
-	for x in $(pactl list sinks | grep "output" | grep "type" | awk '{print $2}'); do
-		echo "$x" | xargs
+	for port in $inactive; do
+		echo "$port"
 	done
+	echo "$active_desc"
 fi
