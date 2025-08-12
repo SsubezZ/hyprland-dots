61b54e16943b98a3e747d02051026666c6246383
SsubezZ <subez.poudel1@gmail.com>
2025-08-12 12:13:05 +0545

    wallpapers: new wallpaper handling logic

--- hypr/scripts/hyprpaper.sh
+++ hypr/scripts/hyprpaper.sh
@@ -1,37 +1,23 @@
 #!/usr/bin/env bash
 
 CONFIG_FILE="$HOME/.config/hypr/wallpapers/wallpapers.conf"
-VAR_FILE="$HOME/.config/hypr/wallpapers/.wallpapers"
+LINK_DIR="$HOME/.config/hypr/wallpapers"
 
-hyprpaper &
+pgrep -x hyprpaper >/dev/null || hyprpaper &
 
 sleep 0.25
 
-WALLPAPER_RIGHT=""
-WALLPAPER_LEFT=""
-
 while IFS='=' read -r monitor wallpaper; do
-	monitor=$(echo "$monitor" | xargs)
-	wallpaper=$(eval echo "$wallpaper")
+  monitor=$(echo "$monitor" | xargs)
+  wallpaper=$(eval echo "$wallpaper")
 
-	# Skip invalid lines
-	if [[ -z "$monitor" || -z "$wallpaper" ]]; then
-		continue
-	fi
+  # Skip invalid lines
+  [[ -z "$monitor" || -z "$wallpaper" ]] && continue
 
-	hyprctl hyprpaper reload "$monitor,$wallpaper"
+  hyprctl hyprpaper reload "$monitor,$wallpaper"
 
-	case "$monitor" in
-	"eDP-1")
-		WALLPAPER_RIGHT="$wallpaper"
-		;;
-	"HDMI-A-1")
-		WALLPAPER_LEFT="$wallpaper"
-		;;
-	esac
+  # Create symlink
+  link_path="$LINK_DIR/background_${monitor}"
+  ln -sf "$wallpaper" "$link_path"
+
 done <"$CONFIG_FILE"
-
-cat >"$VAR_FILE" <<EOF
-export WALLPAPER_RIGHT="$WALLPAPER_RIGHT"
-export WALLPAPER_LEFT="$WALLPAPER_LEFT"
-EOF
