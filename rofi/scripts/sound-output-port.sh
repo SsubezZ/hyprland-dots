3fbdd965efb644d43e5704392cef138ac08c2fb9
SsubezZ <subez.poudel1@gmail.com>
2025-08-17 00:21:26 +0545

    rofi/scripts: audio w/ active selection support

--- rofi/scripts/sound-output-port.sh
+++ rofi/scripts/sound-output-port.sh
@@ -12,13 +12,23 @@ active_desc=$(echo "$ports_info" | awk -v id="$active" '$1 == id {print $2}')
 inactive=$(echo "$ports_info" | awk -v active="$active_desc" '$2 != active {print $2}')
 
 if [ "$#" -gt 0 ]; then
-	desc="$*"
-	device=$(echo "$ports_info" | grep "$desc" | awk '{print $1}' | head -n 1 | xargs)
-	pactl set-sink-port 0 "$device"
+    desc="$*"
+    clean_desc=$(echo "$desc" | sed 's/<[^>]*>//g')
+    device=$(echo "$ports_info" | grep "$clean_desc" | awk '{print $1}' | head -n 1 | xargs)
+    pactl set-sink-port 0 "$device"
 else
-	echo -en "\x00prompt\x1fOutput Port\n"
-	for port in $inactive; do
-		echo "$port"
-	done
-	echo "$active_desc"
+    echo -en "\x00prompt\x1fOutput Port\n"
+    echo -en "\x00markup-rows\x1ftrue\n"
+    if [ -z "$active_desc" ]; then
+        for port in $(echo "$ports_info" | awk '{print $2}'); do
+            echo "$port"
+        done
+    elif [ -z "$inactive" ]; then
+        echo "$active_desc"
+    else
+        for port in $inactive; do
+            echo "$port"
+        done
+        echo "<b><u>$active_desc</u></b>"
+    fi
 fi
